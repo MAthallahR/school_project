2fc569a11a550bb8bcac4af09012221277ed56f4
Athallah <mathallahr37@gmail.com>
2025-02-17 05:36:13 +0700

    Update absen.php

--- Sistem human resource dan penggajian karyawan/karyawan_view/absen.php
+++ Sistem human resource dan penggajian karyawan/karyawan_view/absen.php
@@ -1,22 +1,19 @@
 <?php
 include '../database.php';
 session_start();
-if(!isset($_SESSION['nama'])){
-    header("Location: index.php");
-    exit();
-}
-
 if(isset($_POST['nama'])){
     $nama = $_SESSION['nama'];
     $keterangan = "hadir";
-    $absen_pada = date("Y-m-d H:i:s");
+    $tanggal = date("Y-m-d");
+    $absen_pada = date("H:i:s");
 
-    $sql = "INSERT INTO absensi (nama, keterangan, absen_pada, keluar_pada) VALUES ('$nama', '$keterangan', '$absen_pada', NULL)";
-    if ($db->query($sql)) {
-        echo "Anda absen (checked in).";
+    $sql = "INSERT INTO absensi (nama, keterangan, tanggal, absen_pada, keluar_pada) VALUES ('$nama', '$keterangan', '$tanggal', '$absen_pada', NULL) ON DUPLICATE KEY UPDATE keluar_pada = '$absen_pada'";
+
+    if ($db->query($sql)){
+        $_SESSION['absen'] = true;
         header("Location: karyawan_absensi.php"); 
     } else {
-        echo "Gagal absen: " . $db->error;
+        $db->error;
     }
     exit();
 }
